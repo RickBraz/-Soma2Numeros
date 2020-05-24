ca06da87609258e24a108c8781898fa1c498d6ad
= <=>
2020-05-24 17:42:34 -0300

    adicionei o programa

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _Soma2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intX =  30 ;
+            int intY = 40 ;
+            int intSoma = intX + intY ;
+
+            Console.WriteLine($"{intX} + {intY} = {intSoma}");
+
+        }
+    }
+}
